3fec8e47a87ea22c7acd2fb1db8b278366a443af
Retzam <dretzam@gmail.com>
2021-07-27 02:01:44 +0100

    media bug 13

--- obuntu/settings.py
+++ obuntu/settings.py
@@ -164,8 +164,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_ROOT = '/var/cache/obuntu/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT ,'static')
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = '/var/opt/obuntu/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR ,'media')
 MEDIA_URL = '/media/'
